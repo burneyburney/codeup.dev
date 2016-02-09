23543a039249bcb2beddf24e7c52b122f5a427bc
Michael Burney <michaelburney@Michaels-MacBook-Pro-2.local>
2016-02-08 18:50:26 -0600

    unfinished bom/dom exercise

--- public/defuse_the_bom.php
+++ public/defuse_the_bom.php
@@ -9,7 +9,10 @@
     <button id="defuser">Defuse the BOM</button>
 
     <script>
+    // console.log ("in JS")
         var detonationTimer = 5;
+        // interval
+        var interval = 1000;
 
         // TODO: This function needs to be called once every second
         function updateTimer()
@@ -17,6 +20,7 @@
             if (detonationTimer == 0) {
                 alert('EXTERMINATE!');
                 document.body.innerHTML = '';
+                // clearInterval(intervalID);
             } else if (detonationTimer > 0) {
                 document.getElementById('timer').innerHTML = detonationTimer;
             }
@@ -24,13 +28,27 @@
             detonationTimer--;
         }
 
+
+        var intervalId = setInterval(updateTimer, interval);
+
         // TODO: When this function runs, it needs to
         // cancel the interval/timeout for updateTimer()
         function defuseTheBOM()
         {
-
+            // function updateTimer(){
+            if (detonationTimer > 1){
+                var detonationTimer = 5
+            } else{
+                console.log("wtf did you try to do1?")
+            }
+                // clearInterval(intervalID);            }
+                // var detonationTimer = 0;
+                // return detonationTimer;
+            // timeoutID = setTimeout(function()
+            // function updateTimer(5,5);
+            console.log("you pressed the button");
         }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // Don't modify anything below this line!
         //
         // This causes the defuseTheBOM() function to be called
